0f45d53bbe801664acf0d94011da42f7c0554e2c
kinoshita-neec <kinoshita.tisb@gmail.com>
2024-11-05 09:16:33 +0900

    Add test script for Tello drone flight and rotation

---
+++ test_search_around.py
@@ -0,0 +1,27 @@
+# test_search_around.py
+from djitellopy import Tello
+import time
+
+tello = Tello()
+tello.connect()
+
+tello.takeoff()
+
+# 120 cmに達するまで上昇
+while tello.get_height() < 120:
+    tello.send_rc_control(0, 0, 20, 0)
+    time.sleep(0.2)
+
+# ホバリング
+tello.send_rc_control(0, 0, 0, 0)
+time.sleep(1)
+
+# 360度回転
+tello.rotate_clockwise(360)
+
+# 着陸
+tello.land()
+
+# 接続終了
+tello.end()
+
